f892b4140d87302c5606aea5d43a01fc993ac971
gaogy3 <gaogy3@asiainfo.com>
2020-05-11 21:58:25 +0800

    ggy

--- src/pages/home/components/Bangdan.vue
+++ src/pages/home/components/Bangdan.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
 <el-row>
+<div @click="back" style="height:36px;background-color:rgb(0,188,212);padding-top:6px"><svg t="1589205322820" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2140" width="24" height="24"><path d="M340.154182 535.272727l421.050182-421.050182c9.076364-9.076364 9.076364-23.831273 0-32.907636-9.076364-9.076364-23.831273-9.076364-32.907636 0l-437.480727 437.480727c-9.076364 9.076364-9.076364 23.831273 0 32.907636l437.480727 437.480727c9.076364 9.076364 23.831273 9.076364 32.907636 0 9.076364-9.076364 9.076364-23.831273 0-32.907636L340.154182 535.272727z" p-id="2141" fill="#ffffff"></path></svg></div> 
   <el-col :span="20" v-for="(o, index) in len" :key="o" :offset="index > 0 ? 2 : 2">
     <el-card :body-style="{ padding: '0px'}">
     <div class="bangdan-card-body">
@@ -104,7 +105,9 @@
                   this.news = res.data.data;
               })
           },
-          
+            back:function(){
+                this.$router.push({path:'/'})
+            }
         },
         mounted () {
 		  this.getNewsInfo()
